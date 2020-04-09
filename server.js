655916e71ad51dda091ddbfd0f8874938b99e0a1
gouravchandna <gouravchandna1998@gmail.com>
2020-04-09 05:49:53 +0530

    user list shown

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-const { userJoin, getCurrentUser,userLeave, getRoomUsers} = require('./utils/users');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -24,6 +24,13 @@ io.on('connection', socket => {
 
         //broadcast when a  user connects // to specific room
         socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
+        //send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+
     });
 
     // listen for chatMessage
@@ -31,17 +38,22 @@ io.on('connection', socket => {
     socket.on('chatMessage', msg => {
         const user = getCurrentUser(socket.id);
 
-            io.to(user.room).emit('message', formatMessage(user.username, msg));
-        
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+
 
     });
     //runs when client disconnects
     socket.on('disconnect', () => {
-        const user =userLeave(socket.id);
+        const user = userLeave(socket.id);
 
-        if(user){
-            
+        if (user) {
+
             io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
         }
 
 
